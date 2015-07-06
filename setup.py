96ec28a4c1339fcffd8dd1e2421f77b46d702b87
Alberto Lorenzo <alorenzo.md@gmail.com>
2015-07-06 22:12:39 +0200

    Tavis CI (5th attempt)
    OpenMP not supported for OS X in Travis CI.

--- setup.py
+++ setup.py
@@ -20,12 +20,16 @@ include_path = './include'
 copt = {'mingw32': ['-fopenmp', '-O3'],
         'mingw64': ['-fopenmp', '-O3'],
         'cygwin': ['-fopenmp', '-O3'],
-        #'msvc': ['/openmp', '/Ox'],    OpenMP not support by Appveyor
+        'msvc': ['/openmp', '/Ox'],
         'unix': ['-fopenmp', '-O3']}
 lopt = {'mingw32': ['-lgomp'],
         'mingw64': ['-lgomp'],
         'cygwin': ['-lgomp'],
         'unix': ['-lgomp']}
+
+if 'TRAVIS' in os.environ or 'APPVEYOR' in os.environ:
+    copt = {}
+    lopt = {}
 
 
 class build_subclass(build):
