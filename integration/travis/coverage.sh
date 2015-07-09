c1d0f5aa39acb91617ac669ae4cf1003498ab40d
Alberto Lorenzo <alorenzo.md@gmail.com>
2015-07-09 16:20:32 +0200

    Coveralls (7th attempt)

--- integration/travis/coverage.sh
+++ integration/travis/coverage.sh
@@ -1,6 +1,7 @@
 source activate _test
 pip install pytest-cov
+pip install python-coverage
 export SP=$(python -c "import site;print(site.getsitepackages()[0])")
 cp .coveragerc $SP/pysapp/
-cd $SP
-py.test --cov pysapp pysapp/test --cov-config pysapp/.coveragerc
+py.test --cov $SP/pysapp $SP/pysapp/test --cov-config $SP/pysapp/.coveragerc
+cp $SP/pysapp/.coverage ./
