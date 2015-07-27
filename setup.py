eb684d72f144f316bd5c473ead58a54fd33bee1a
Alberto Lorenzo <alorenzo.md@gmail.com>
2015-07-27 20:25:47 +0200

    More silly fixes
    No comments.

--- setup.py
+++ setup.py
@@ -50,21 +50,23 @@ if any(build_ids.values()):
         file.write(filedata)
 
 # Download numpy.i file
-version = re.compile(r'(?P<MAJOR>[0-9]+)\.'
-                     '(?P<MINOR>[0-9]+)') \
-                     .search(numpy.__version__)
-version_string = version.group()
-version_info = {key: int(value) for key, value in version.groupdict().items()}
+np_version = re.compile(r'(?P<MAJOR>[0-9]+)\.'
+                        '(?P<MINOR>[0-9]+)') \
+                        .search(numpy.__version__)
+np_version_string = np_version.group()
+np_version_info = {key: int(value)
+                   for key, value in np_version.groupdict().items()}
 
-file_name = 'numpy.i'
-file_url = 'https://raw.githubusercontent.com/numpy/numpy/maintenance/' + \
-           version_string + '.x/tools/swig/' + file_name
-if(version_info['MAJOR'] == 1 and version_info['MINOR'] < 9):
-    file_url.replace('tools', 'doc')
+np_file_name = 'numpy.i'
+np_file_url = 'https://raw.githubusercontent.com/numpy/numpy/maintenance/' + \
+              np_version_string + '.x/tools/swig/' + np_file_name
+if(np_version_info['MAJOR'] == 1 and np_version_info['MINOR'] < 9):
+    np_file_url.replace('tools', 'doc')
 
 chunk_size = 8196
-with open(file_name, 'wb') as file:
-    for chunk in requests.get(file_url, stream=True).iter_content(chunk_size):
+with open(np_file_name, 'wb') as file:
+    for chunk in requests.get(np_file_url,
+                              stream=True).iter_content(chunk_size):
         file.write(chunk)
 
 
