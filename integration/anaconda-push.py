ba01d85b58d1afe73160b58790385b012a604eb7
Alberto Lorenzo <alorenzo.md@gmail.com>
2015-08-02 22:16:26 +0200

    Trial and error learning
    I don’t like to try new things on production branches but it is
    necessary since they’re the branches to be uploaded.

--- integration/anaconda-push.py
+++ integration/anaconda-push.py
@@ -27,7 +27,7 @@ if(branch):
         shutil.move(binary_package, '.')
 
         token = os.environ['ANACONDA_TOKEN']
-        cmd = ['anaconda', 'upload', '-t', token, '--force']
+        cmd = ['anaconda', '--token', token, 'upload', '--force']
         cmd.extend(glob.glob('*.tar.bz2'))
         try:
             subprocess.check_call(cmd)
