917a215e77755628925ce3e7395740db640e44fe
Alberto Lorenzo <alorenzo.md@gmail.com>
2015-12-08 01:23:20 +0100

    CI test scripts cleaned up
    Attempt to simplify the scripts

--- integration/appveyor/run_with_env.cmd
+++ integration/appveyor/run_with_env.cmd
@@ -6,7 +6,7 @@
 :: variables to use the MSVC 2008 C++ compilers from GRMSDKX_EN_DVD.iso of:
 :: MS Windows SDK for Windows 7 and .NET Framework 3.5 (SDK v7.0)
 ::
-:: 32 bit and 64 bit Python 3.5 builds do not require specific environment
+:: 32 bit (and 64 bit Python 3.5) builds do not require specific environment
 :: configurations.
 :: 
 :: Note: this script needs to be run with the /E:ON and /V:ON flags for the
@@ -18,35 +18,36 @@
 ::
 :: Author: Olivier Grisel
 :: License: CC0 1.0 Universal: http://creativecommons.org/publicdomain/zero/1.0/
+:: Modified by: Alberto Lorenzo
+
 @ECHO OFF
-
 SET COMMAND_TO_RUN=%*
-SET WIN_SDK_ROOT=C:\Program Files\Microsoft SDKs\Windows
+SET SDK_ROOT=C:\Program Files\Microsoft SDKs\Windows
 
-SET MAJOR_PYTHON_VERSION="%PYTHON_VERSION:~0,1%"
-IF %MAJOR_PYTHON_VERSION% == "2" (
-    SET WINDOWS_SDK_VERSION="v7.0"
-) ELSE IF %MAJOR_PYTHON_VERSION% == "3" (
-    SET WINDOWS_SDK_VERSION="v7.1"
+SET MAJOR="%PYTHON:~0,1%"
+IF %MAJOR% == "2" (
+    SET WIN_SDK="v7.0"
+) ELSE IF %MAJOR% == "3" (
+    SET WIN_SDK="v7.1"
 ) ELSE (
-    ECHO Unsupported Python version: "%MAJOR_PYTHON_VERSION%"
+    ECHO Unsupported Python version: "%MAJOR%"
     EXIT 1
 )
 
-IF %PYTHON_ARCH%=="32" (
-    ECHO Using default MSVC build environment for 32 bit architecture
+IF %ARCH%=="32" (
+    ECHO Using default MSVC build environment for 32 bit arch
     ECHO Executing: %COMMAND_TO_RUN%
     call %COMMAND_TO_RUN% || EXIT 1
-) ELSE IF %PYTHON_VERSION%==3.5 (
-    ECHO Using default MSVC build environment for 64 bit architecture
+) ELSE IF %PYTHON%=="3.5" (
+    ECHO Using default MSVC build environment for 64 bit arch
     ECHO Executing: %COMMAND_TO_RUN%
     call %COMMAND_TO_RUN% || EXIT 1
 ) ELSE (
-    ECHO Configuring Windows SDK %WINDOWS_SDK_VERSION% for Python %MAJOR_PYTHON_VERSION% on a 64 bit architecture
+    ECHO Configuring Windows SDK %WIN_SDK% for Python %MAJOR% on a 64 bit arch
     SET DISTUTILS_USE_SDK=1
     SET MSSdk=1
-    "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Setup\WindowsSdkVer.exe" -q -version:%WINDOWS_SDK_VERSION%
-    "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Bin\SetEnv.cmd" /x64 /release
+    "%SDK_ROOT%\%WIN_SDK%\Setup\WindowsSdkVer.exe" -q -version:%WIN_SDK%
+    "%SDK_ROOT%\%WIN_SDK%\Bin\SetEnv.cmd" /x64 /release
     ECHO Executing: %COMMAND_TO_RUN%
     call %COMMAND_TO_RUN% || EXIT 1
 )
--- integration/travis/install.sh
+++
@@ -1,6 +0,0 @@
-MINICONDA_URL="http://repo.continuum.io/miniconda"
-MINICONDA_FILE="Miniconda3-latest-MacOSX-x86_64.sh"
-wget "${MINICONDA_URL}/${MINICONDA_FILE}"
-bash $MINICONDA_FILE -b
-
-export PATH=$HOME/miniconda3/bin:$PATH
